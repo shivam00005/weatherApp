250d5b8e56fcb9be28d5b876f066eb6fbc5d749d
Shivam Sharma <shivampramod95925@gmail.com>
2023-01-27 09:59:37 -0600

    added background change function which changes background image to day and night according live time

--- script/app.js
+++ script/app.js
@@ -1,5 +1,21 @@
 import curentWeather from "./functions.js"
 import { hourlyCards } from "./functions.js"
 
+// change background function
+
+function backgroundChange() {
+
+    let time = new Date();
+    let hours = time.getHours();
+
+    if (hours > 6) {
+        document.body.style.backgroundImage = "url('images/day.jpg')";
+    } else {
+        document.body.style.backgroundImage = "url('images/night.jpg')";
+
+    }
+
+}
+backgroundChange();
 curentWeather();
 hourlyCards();
