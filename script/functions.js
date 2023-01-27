664c3c9ac6c4ecba3114befa8e9e8b7640ce5de1
Shivam Sharma <shivampramod95925@gmail.com>
2023-01-26 23:46:33 -0600

    added hourlyCards function to function.js  and imported app.js
    - hourlyCard function includes the creation of a bunch of HTML elements using a document.createElement
    - used For loop to create 24cards that showcase day temperature hourly.

--- script/functions.js
+++ script/functions.js
@@ -54,4 +54,83 @@ const curentWeather = () => {
 
 }
 
+export function hourlyCards() {
+
+    let output = document.getElementById('output');
+
+    let dayTimeCard = document.createElement('div');
+    dayTimeCard.className = "dayTimeCard";
+
+    dayTimeCard.innerHTML = `<h2>Regina hourly Weather Report</h2>`;
+
+
+    let Cards = document.createElement('div');
+    Cards.className = 'wrapper';
+
+
+    let dayCard = document.createElement('ol');
+    dayCard.className = "dayCard carousel";
+    dayCard.setAttribute('data-target', 'carousel')
+
+
+
+
+    for (let i = 1; i < 25; i++) {
+
+        let card = document.createElement('li');
+        card.className = 'card';
+        card.setAttribute('data-target', 'card');
+
+        let timecard = document.createElement('div');
+        timecard.className = 'timecard';
+
+        let img = document.createElement('img');
+        img.setAttribute('src', 'images/tempImg.png');
+        img.setAttribute('alt', 'current weather icon');
+
+        let temprature = document.createElement('h2')
+        temprature.className = 'temp';
+        temprature.innerText = '7°';
+
+        let dayTime = document.createElement('span')
+        dayTime.className = 'time';
+
+        i > 12 ? dayTime.innerText = i + ' Pm ' : dayTime.innerText = i + ' Am ';
+
+
+        timecard.appendChild(img);
+        timecard.appendChild(temprature);
+        timecard.appendChild(dayTime);
+
+        card.appendChild(timecard);
+
+        dayCard.appendChild(card);
+        Cards.appendChild(dayCard);
+
+
+    }
+
+    let buttonWrapper = document.createElement('div');
+    buttonWrapper.className = 'button-wrapper';
+
+    let leftBtn = document.createElement('button');
+    leftBtn.setAttribute('id', 'left-button');
+    leftBtn.innerText = '<';
+
+    let rightBtn = document.createElement('button');
+    rightBtn.setAttribute('id', 'right-button');
+    rightBtn.innerText = '>';
+
+    buttonWrapper.appendChild(leftBtn);
+    buttonWrapper.appendChild(rightBtn);
+
+    Cards.appendChild(buttonWrapper);
+
+    dayTimeCard.appendChild(Cards);
+    output.appendChild(dayTimeCard);
+
+
+
+}
+
 export default curentWeather;
